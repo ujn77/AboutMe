4b8a19ef17117a3fbd680508ce982498627b2754
Alexey Buynichev <lex1977@gmail.com>
2016-12-30 12:58:57 +0700

    add menu

--- app/src/main/java/com/example/abuinichev/lesson2/MainActivity.java
+++ app/src/main/java/com/example/abuinichev/lesson2/MainActivity.java
@@ -1,23 +1,48 @@
 package com.example.abuinichev.lesson2;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //ll = (LinearLayout) findViewById(R.layout.main_activity);
         setContentView(R.layout.main_activity);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-		/* Inflate the menu; this adds items to the action bar if it is present */
+        /* Inflate the menu; this adds items to the action bar if it is present */
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mn_color_red:
+                //tvText.setText(edText.getText());
+                //ll = (LinearLayout) findViewById(R.id.main_activity);
+                //ll.setBackgroundColor(Color.RED);
+                Toast toast = Toast.makeText(this, "Просто тост", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
